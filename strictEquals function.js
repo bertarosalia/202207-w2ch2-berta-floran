1e2f9c899505866e8dea67067ed85b44df9eff9b
Berta Floran <bertafloran@hotmail.com>
2022-07-12 20:30:18 +0200

    second condition tested

--- strictEquals function.js
+++ strictEquals function.js
@@ -1,8 +1,11 @@
 const strictEquals = (a, b) => {
   if (Object.is(a, NaN) && Object.is(b, NaN)) {
     return false;
+  } else if (!a && !b) {
+    return false;
   }
+
   return Object.is(a, b);
 };
 
-strictEquals(NaN, NaN);
+strictEquals(-0, 0);
