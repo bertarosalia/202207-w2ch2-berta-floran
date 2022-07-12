bf0c388118afe93ad12d550b7eb86c31c9cf6765
Berta Floran <bertafloran@hotmail.com>
2022-07-12 20:48:16 +0200

    test done with 1 and 1

--- strictEquals function.js
+++ strictEquals function.js
@@ -1,7 +1,7 @@
 const strictEquals = (a, b) => {
   if (Object.is(a, NaN) && Object.is(b, NaN)) {
     return false;
-  } else if (!a && !b) {
+  } else (!a && !b) {
     return true;
   }
   return Object.is(a, b);
